cf97cef2fd9af2312aada4bb3faf1590c341ccfc
Rincevent42 <porvooc@gmail.com>
2020-04-06 12:59:34 +0200

    feat: add online status toggling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import './Contact.css';
 
-/*
-const name = 'Harper Rice';
-const avatar = 'https://randomuser.me/api/portraits/women/83.jpg';
-const online = true;
-*/
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    }
+  }
+  render() { 
+    return (
+      <div className='Contact'>
+        <img className='avatar' src={this.props.avatar} alt={this.props.name} />
 
-const Contact = (props) => (
-  <div className='Contact'>
-    <img className='avatar' src={props.avatar} alt={props.name} />
-
-    <div>
-      <div className='name'>
-        {props.name}
-      </div>
-      <div className='status'>
-        <span className={props.online ? 'status-online' : 'status-offline'} />
-        <div className='status-text'>
-          {(props.online) ? 'online' : 'offline'}
+        <div>
+          <div className='name'>
+            {this.props.name}
+          </div>
+          <div className='status'
+            onClick={event => {
+              const newStatus = !this.state.online;
+              this.setState({online: newStatus});
+            }}
+          >
+            <span className={this.state.online ? 'status-online' : 'status-offline'}/>
+            <div className='status-text'>
+              {(this.state.online) ? 'online' : 'offline'}
+            </div>
+          </div>
         </div>
+
       </div>
-    </div>
-
-  </div>
-);
-
+    );
+  }
+}
+ 
 export default Contact;
+
